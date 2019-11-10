e375224fba2c5fb0c12d2fd9af205725199e4bca
Christopher McArthur <prince.chrismc@gmail.com>
2019-11-09 19:49:24 -0500

    quick setup script for conan

---
+++ setup-conan.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+pip3 install conan
+
+# Add remote for conan to find RESTinio package.
+conan remote add stiffstream https://api.bintray.com/conan/stiffstream/public
+# Add remote for conan to find RESTinio's dependencies.
+conan remote add public-conan https://api.bintray.com/conan/bincrafters/public-conan
+
